91722b9f3eb805af659b19b78cb0a9d08effac17
Eriarer <eriarer_messiah@hotmail.com>
2023-05-20 15:36:00 -0600

    subshells

---
+++ menus/opcion2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+clear
+echo "estas en un subShell"
+echo "Opcion 2"
+echo ""
+echo ""
+echo "Presione enter para continuar"
+read -sn 1
---
+++ menus/opcion3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+clear
+echo "estas en un subShell"
+echo "Opcion 3"
+echo ""
+echo ""
+echo "Presione enter para continuar"
+read -sn 1
