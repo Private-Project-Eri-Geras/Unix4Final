63b1e387bcd83c57cf4d3beef52e8546da03261d
Eriarer <eriarer_messiah@hotmail.com>
2023-05-25 18:18:08 -0600

    selector de archivos

--- usuarios/bajas/masiva.sh
+++ usuarios/bajas/masiva.sh
@@ -1,1 +1,23 @@
 #!/usr/bin/env bash
+while true; do
+    archivo_usuarios=$(dialog --title --cursor-off-label "Selecciona un archivo" \
+        --stdout \
+        --fselect /home/ 14 70)
+    archivo_usuario_Output=$?
+    # Si el usuario presiona "Cancel" se sale del script
+    if [ $archivo_usuario_Output -eq 1 ]; then
+        break
+    fi
+    if [ -f "$archivo_usuarios" ]; then
+        dialog --title "CONFIRMAR" --yesno "¿Deseas usar el archivo $archivo_usuarios?" 0 0
+        dialog_Output=$?
+        if [ $dialog_Output -eq 0 ]; then
+            # Si el usuario presiona "Yes" se ejecuta la función addUsers
+            clear
+            addUsers "$archivo_usuarios"
+            break # Salir del ciclo while después de confirmar
+        fi
+    else
+        break # Salir del ciclo while si no se selecciona un archivo
+    fi
+done
