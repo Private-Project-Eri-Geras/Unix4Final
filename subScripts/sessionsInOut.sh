efd5fc0280af7f04f07a627efa2bc31593a424a0
mariana-avila-rivera <marianaavilarivera@gmail.com>
2023-05-26 21:22:48 -0600

    Da resultado de DIalog Box, el btn no funciona

--- subScripts/sessionsInOut.sh
+++ subScripts/sessionsInOut.sh
@@ -1,19 +1,47 @@
 #!/bin/bash
-#Ruta del archivo que guarda los inicios y terminos de sesion
+# Ruta del archivo que guarda los inicios y terminos de sesión
 new=/tmp/wwho1$$.txt
 old=/tmp/wwho2$$.txt
 diferencia=/tmp/diff$$.txt
-#Guardando la lista de usuarios conectados
-who>$old
+archivo=/tmp/usuarios.txt
+# Función para mostrar un cuadro de diálogo con el contenido del archivo
+mostrar_cuadro_dialogo() {
+    dialog --title "Usuarios" --textbox "$1" 0 0 
+        
+}
 
-#Detección de inicios y terminos de sesion
-while test 1 == 1
+# Guardando la lista de usuarios conectados
+who > $old
+
+#Creando el cuadro de dialogo 
+touch $archivo
+mostrar_cuadro_dialogo $archivo
+
+# Detección de inicios y terminos de sesión
+while true
 do
-  who>$new
+  who > $new
   diff $old $new > $diferencia
-  cat $diferencia | awk  '/>/ { print "in:   " $0 ; }' 
-  cat $diferencia | awk  '/</ { print "out:  " $0 ; }'
+
+  # Obtener los usuarios que entran y mostrarlos en el cuadro de diálogo
+  usuarios_entran=$(awk '/>/ { print "in:   " $0 ; }' $diferencia)
+  if [ -n "$usuarios_entran" ]; then
+      echo "$usuarios_entran" >> /tmp/usuarios.txt
+     press= mostrar_cuadro_dialogo "/tmp/usuarios.txt"
+     if [ "$dialog_output" = "Aceptar" ]; then
+        #Sale de la lista desplegable de usuarios
+          break
+      fi
+  fi
+
+  # Obtener los usuarios que salen y mostrarlos en el cuadro de diálogo
+  usuarios_salen=$(awk '/</ { print "out:  " $0 ; }' $diferencia)
+  if [ -n "$usuarios_salen" ]; then
+      echo "$usuarios_salen" >> /tmp/usuarios.txt
+      mostrar_cuadro_dialogo "/tmp/usuarios.txt"
+  fi
+
   mv $new $old
-  sleep 10
+  sleep 1
 done
 
