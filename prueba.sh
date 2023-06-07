4dcf134981c18e9b98318c65eab205a902769fad
mariana-avila-rivera <marianaavilarivera@gmail.com>
2023-06-07 15:43:26 -0600

    probando detección de tecla. ERROR

--- prueba.sh
+++ prueba.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-function DialogGen(){
-    dialog --title "Hello" --msgbox 'Hello GFG Users!' 6 20
+
+# Función que se ejecuta al presionar Enter
+function accion_enter() {
+    # Realiza la acción que deseas al presionar Enter
+    echo "Presionaste Enter"
+    exit 0  # Sale del script
 }
-DialogGen
+
+# Muestra el cuadro de texto
+
+dialog --title "Cuadro de Texto" --textbox archivo.txt 0 0 \
+    --and-widget --ok-label "Enter" --textbox-cb "accion_enter"
+
+# Limpia la pantalla después de cerrar el cuadro de texto
+clear
