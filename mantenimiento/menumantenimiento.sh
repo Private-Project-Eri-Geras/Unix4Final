8366c864bfacdef34789fd524b5899c5a59e069a
Laxlev <leofrivgames1@gmail.com>
2023-06-07 14:44:56 -0600

    otropuch

---
+++ mantenimiento/menumantenimiento.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Define the options
+options=(
+    1 "Reinicio en modo Manual"
+    2 "Tareas de mantenimiento"
+    3 "Option 3"
+    4 ""
+    5 ""
+)
+
+# Initialize the selected option
+selected=0
+
+# Clear the screen
+clear
+
+# Print the menu using dialog
+while true; do
+    # Mostrar el menu y cambiar el valor de la variable $selected
+    selected=$(dialog --clear --title "MENU PRINCIPAL" \
+        --cancel-label "Exit" --ok-label "Select" \
+        --menu "Seleccione una opción:" 0 0 0 "${options[@]}" \
+        --output-fd 1)
+
+    # Exit if the user presses cancel
+    if [[ $? -ne 0 ]]; then
+        break
+    fi
+
+    # Handle the selected option
+    case $selected in
+    1)
+        (source "usuarios/menuUsuarios.sh")
+        ;;
+    2)
+        (source "mantenimiento/manualreboot.sh")
+        ;;
+    3)s
+        source "mantenimiento/opcion3.sh"
+        ;;
+    esac
+
+    # Clear the screen
+    clear
+done
+
+# Exit the script
+clear
+exit
