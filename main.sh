ab7547ffc566a50058c8c11fa668a4b62cbee28c
diosescalera <al217108@edu.uaa.mx>
2023-06-07 14:09:13 -0600

    Menu Principal

---
+++ main.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+# Función para mostrar la ventana de ayuda
+mostrar_ayuda() {
+    dialog --title "Help" --msgbox "aqui esta la ayuda" 0 0
+}
+
+# Verificar si el script se ejecuta con sudo
+if [ -z "$SUDO_USER" ]; then
+    dialog --colors --title "\Z1ERROR" --msgbox "Este script debe ser ejecutado con sudo" 0 0
+    clear
+    # exit 1
+fi
+
+# Define las opciones del menú
+options=(
+    1 "Programación de tareas manual"
+    2 "Respaldo programado"
+    3 "Borrado de temporales programado"
+    4 "Inhabilitación de usuarios (por periodo de tiempo)"
+)
+
+# Limpia la pantalla
+clear
+
+# Imprime el menú usando dialog
+while true; do
+    # Muestra el menú y cambia el valor de la variable $option
+    # --ok-label = 0
+    # --cancel-label = 1
+    # --help-button --help-label = 2
+    # --extra-button --extra-label = 3
+    option=$(dialog --cursor-off-label --colors --clear --title "ADMINISTRACION DE USUARIOS" \
+        --cancel-label "Cancelar" --ok-label "Seleccionar" \
+        --help-button --help-label "Ayuda" \
+        --menu "Seleccione una opción:" 0 0 0 "${options[@]}" \
+        --output-fd 1)
+
+    dialog_exit_code=$?
+
+    # Verificar si el usuario seleccionó el botón de ayuda
+    if [[ "$dialog_exit_code" -eq 2 ]]; then
+        mostrar_ayuda
+        continue
+    fi
+
+    # Verificar si el usuario seleccionó cancelar
+    if [[ "$dialog_exit_code" -eq 1 ]]; then
+        break
+    fi
+
+    # Manejar la opción seleccionada
+    case $option in
+    1)
+        # (source "")
+        ;;
+    2)
+        (source "respaldoProgramado/menuRespaldo.sh")
+        ;;
+    3)
+        # (source "")
+        ;;
+    4)
+        # (source "")
+        ;;
+    *)
+        dialog --colors --title "\Z1ERROR" --msgbox "Opción inválida" 0 0
+        ;;
+    esac
+
+    # Limpia la pantalla
+    clear
+done
+
+# Sale del script
+clear
