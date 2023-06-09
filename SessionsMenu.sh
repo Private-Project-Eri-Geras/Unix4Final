1047bda9ddd0db4e931aed71aac230e589343117
mariana-avila-rivera <marianaavilarivera@gmail.com>
2023-06-08 18:42:22 -0600

    Otra opción en menu

--- SessionsMenu.sh
+++ SessionsMenu.sh
@@ -2,11 +2,11 @@
 
 #Define the options
 opt=(
-    1 "Respaldos carpetas"
-    2 "Tiempo de sesión"
-    3 "Sincronizar carpeta"
+    1 "Inicio/Termino de sesión"
+    2 "Respaldos carpetas"
+    3 "Tiempo de sesión"
+    4 "Sincronizar carpeta"
 )
-
 #Initialize the selected option
 selected=0
 
@@ -14,18 +14,36 @@ selected=0
 clear
 
 #Print the menu using dialog
-    while true; 
-    do
+    while true; do
         #Mostrar le menu y cambiar el valor de la variable "$selected"
-        selected=$(dialog --clear --title "Menu Sessions"\
-        --cancel-label "Cancel" --ok-label "Select" \
+        selected=$(dialog --cursor-off-label --colors --clear --title "Menu Sessions"\
+        --cancel-label "Cancelar" --ok-label "Seleccionar" \
         --menu "Seleccione una opción:" 0 0 0 "${opt[@]}" \
         --output-fd 1)
+            # ancho, alto, alto del menu interno
+        dialogExit=$?
 
         #Exit if the user presses cancel
-        if [[ $? -ne 0 ]]; then
+        if [[ "dialogExit" -eq 1 ]]; then
             break
         fi
+
+        #Si se selecciono una de las opciones:
+        case $selected in
+        1)
+            (source "subScripts/sessionsInOut.sh")
+            ;;
+        2)
+            ;;
+        3)
+            ;;
+        4)
+            ;;
+        *)
+            dialog --colors --title "\Z1ERROR" --msgbox "Opción inválida" 0 0
+            ;;
+        esac
+        clear
     done
 
 #Exit the script
