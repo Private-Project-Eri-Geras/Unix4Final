49a120b454567cce2f0b1c496301189e17ef2007
mariana-avila-rivera <marianaavilarivera@gmail.com>
2023-06-08 20:02:38 -0600

    Imprime los usuarios y su tiempo en sesión H:M:S

--- prueba.sh
+++ prueba.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-# Función que se ejecuta al presionar Enter
-function accion_enter() {
-    # Realiza la acción que deseas al presionar Enter
-    echo "Presionaste Enter"
-    exit 0  # Sale del script
-}
+# Obtener la lista de usuarios conectados
+who | awk '{print $1,$4,$5}' | while read user login time
+do
+    # Calcular el tiempo en sesión
+    seconds=$(($(date +%s) - $(date -d "$login $time" +%s)))
+    formatted_time=$(date -u -d @${seconds} +"%H:%M:%S")
 
-# Muestra el cuadro de texto
-
-dialog --title "Cuadro de Texto" --textbox archivo.txt 0 0 \
-    --and-widget --ok-label "Enter" --textbox-cb "accion_enter"
-
-# Limpia la pantalla después de cerrar el cuadro de texto
-clear
+    # Imprimir el usuario y el tiempo en sesión
+    echo "Usuario: $user - Tiempo en sesión: $formatted_time"
+done
