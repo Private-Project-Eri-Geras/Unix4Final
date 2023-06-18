425aad2c63a609db6e62e41a075693bec17b7c97
mariana-avila-rivera <marianaavilarivera@gmail.com>
2023-06-18 01:13:38 -0600

    con dialogSelect

--- glam-package/usr/src/glam/tareasUsuarios/subScripts/configRespaldo.sh
+++ glam-package/usr/src/glam/tareasUsuarios/subScripts/configRespaldo.sh
@@ -6,47 +6,81 @@ opt=(
 )
 ###############################################################
 #FUNCIONES
+configResp(){
+    usr=$1  #Guarda el usuario seleccionado
+
+    #Guarda la carpeta/archivo a respaldar al iniciar sesión el usuario
+    origen=$(dialog --title "Selecciona un archivo o directorio a respaldar" \
+        --cancel-label "Cancelar" \
+        --help-button --help-label "Ayuda" \
+        --stdout --cursor-off-label --fselect /home/ 14 70)
+    opcion=$?
+
+   
+    if [[ "usuario" -eq 1 ]]; then  #Exit if the user presses cancel
+        return
+    fi
+
+    #Guarda la ruta para guardar el respaldo
+    destino=$(dialog --title "Selecciona un directorio para guardar el respaldo" \
+        --cancel-label "Cancelar" \
+        --help-button --help-label "Ayuda" \
+        --stdout --cursor-off-label --dselect /home/ 14 70)
+    opcion=$?
+
+    if [[ "usuario" -eq 1 ]]; then  #Exit if the user presses cancel
+        return
+    fi
+
+    echo -e "Ruta arch/dir a respaldar: $origen \n Ruta destino: $destino"
+    sleep 20
+    return
+}
 
 
 #subMenu verARch/configurar (Se meustra lista de usuarios)
 verArchRespaldos() {
-
+    
     return
 }
 
 menuUsrs() {
    # Obtener todos los usuarios registrados en el sistema (excluyendo los usuarios del sistema)
-usuarios=$(grep -E '^[^:]+:[^:]*:[0-9]{4}:[0-9]{4}' /etc/passwd | cut -d: -f1)
+    usuarios=$(grep -E '^[^:]+:[^:]*:[0-9]{4}:[0-9]{4}' /etc/passwd | cut -d: -f1)
 
-# Verificar si no hay usuarios creados
-if [ -z "$usuarios" ]; then
-  #echo "No se encontraron usuarios creados en el sistema."
-  return
-fi
-
-# Convertir los usuarios en un arreglo
-usuarios_array=($usuarios)
-
-# Crear un arreglo de opciones para el menú
-opciones=()
-for usuario in "${usuarios_array[@]}"; do
-    opciones+=("$usuario" "")
-done
-
-# Mostrar el menú y guardar la opción seleccionada
-usuario=$(dialog --cursor-off-label --colors --clear --title "Configuración de tiempo en sesión" --cancel-label "Cancelar" --ok-label "Seleccionar" --menu "Seleccione un usuario:" 0 0 0 "${opciones[@]}" 2>&1 >/dev/tty)
-
-#Exit if the user presses cancel
-if [[ "usuario" -eq 1 ]]; then
+    # Verificar si no hay usuarios creados
+    if [ -z "$usuarios" ]; then
+    #echo "No se encontraron usuarios creados en el sistema."
     return
-fi
+    fi
+
+    # Convertir los usuarios en un arreglo
+    usuarios_array=($usuarios)
+
+    # Crear un arreglo de opciones para el menú
+    opciones=()
+    for usuario in "${usuarios_array[@]}"; do
+        opciones+=("$usuario" "")
+    done
+
+    # Mostrar el menú y guardar la opción seleccionada
+    usuario=$(dialog --cursor-off-label --colors --clear --title \
+    "Configuración de tiempo en sesión" --cancel-label "Cancelar" \
+    --ok-label "Seleccionar" --menu "Seleccione un usuario:" 0 0 0 "${opciones[@]}" 2>&1 >/dev/tty)
+
+    #Exit if the user presses cancel
+    if [[ "usuario" -eq 1 ]]; then
+        return
+    fi
+
+    configResp "$usuario"
 }
 
 ################################################################
 #Print the menu using dialog
     while true; do
         #Mostrar le menu y cambiar el valor de la variable "$selected"
-        selected=$(dialog --cursor-off-label --colors --clear --title "Respaldo de carpetas al iniciar sesión"\
+        selected=$(dialog --cursor-off-label --colors --clear --title "Respaldos al iniciar sesión"\
         --cancel-label "Cancelar" --ok-label "Seleccionar" \
         --menu "Seleccione una opción:" 0 0 0 "${opt[@]}" \
         --output-fd 1)
