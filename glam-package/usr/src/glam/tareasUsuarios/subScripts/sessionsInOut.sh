fe743eda90fd5d8a023acb54c9c83f385980ee1c
mariana-avila-rivera <marianaavilarivera@gmail.com>
2023-06-19 15:00:43 -0600

    lee el archivo con los usuarios (InOut)

--- glam-package/usr/src/glam/tareasUsuarios/subScripts/sessionsInOut.sh
+++ glam-package/usr/src/glam/tareasUsuarios/subScripts/sessionsInOut.sh
@@ -1,51 +1,20 @@
 #!/bin/bash
-# Ruta del archivo que guarda los inicios y terminos de sesión
-new=/var/glam/tmp/newWho.txt
-old=/var/glam/tmp/oldWho.txt
-diferencia=/var/glam/tmp/diff.txt
 nameArch="usuarios$(date +'%d%m%y').txt"
-ruta="/var/glam/logs/usrsInOut" 
-rutaUsrs="$ruta/$nameArch"
-##### FUNCIONES ####
-  mkdir -p $ruta #verifica si existe la ruta, si no existe la crea
-  touch $rutaUsrs
-# Función para mostrar un cuadro de diálogo con las últimas 10 líneas del archivo
+  ruta="/var/glam/logs/usrsInOut" 
+  rutaUsrs="$ruta/$nameArch"
+
 mostrar_cuadro_dialogo() {
   dialog --title "Usuarios" --backtitle "q para salir" --infobox "$( tail $rutaUsrs )" 0 0
   dialogExit=$?
 }
 
-##### CÓDIGO #####
-
-# Guardando la lista de usuarios conectados
-who > "$old"
-
-# Creando el cuadro de diálogo
-# Eliminar el archivo temporal
-touch "$rutaUsrs"
-mostrar_cuadro_dialogo "$rutaUsrs" &
-
 # Detección de inicios y terminos de sesión
 while true
 do
-  who > "$new"
-  diff "$old" "$new" > "$diferencia"
-
-  # Obtener los usuarios que entran y mostrarlos en el cuadro de diálogo
-  usuarios_entran=$(awk '/>/ { print "in:   " $0 ; }' "$diferencia")
-  if [ -n "$usuarios_entran" ]; then
-    echo "$usuarios_entran" >> $rutaUsrs
-    mostrar_cuadro_dialogo
-  fi
-
-  # Obtener los usuarios que salen y mostrarlos en el cuadro de diálogo
-  usuarios_salen=$(awk '/</ { print "out:  " $0 ; }' "$diferencia")
-  if [ -n "$usuarios_salen" ]; then
-      echo "$usuarios_salen" >> $rutaUsrs
-      mostrar_cuadro_dialogo 
-  fi
-
-  mv "$new" "$old"
+  nameArch="usuarios$(date +'%d%m%y').txt"
+  ruta="/var/glam/logs/usrsInOut" 
+  rutaUsrs="$ruta/$nameArch"
+  mostrar_cuadro_dialogo
 
   # Leer la tecla presionada por el usuario
     read -rsn1 -t 1 key
@@ -56,9 +25,6 @@ do
     fi
 
 done
-rm $new
-rm $old
-rm $diferencia
 
 #Exit the script
 clear
