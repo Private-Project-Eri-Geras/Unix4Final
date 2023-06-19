7af6cf56e9a079a7b049603798f2896e9548a75d
diosescalera <al217108@edu.uaa.mx>
2023-06-19 02:34:53 -0600

    DONE

--- glam-package/usr/src/glam/programacionTareas/respaldoProgramado/borrar/borrarRespaldoPlantilla.sh
+++ glam-package/usr/src/glam/programacionTareas/respaldoProgramado/borrar/borrarRespaldoPlantilla.sh
@@ -12,13 +12,15 @@ clear
 
 # Imprime el menú usando dialog
 while true; do
-    OPTIONS=$(dialog --no-tags --separate-output --clear --title "Selección de Respaldos" \
-        --checklist "Use Espacio para seleccionar los respaldos y pulse OK cuando haya terminado." 0 0 0 \
+    OPTIONS=$(
+        dialog --no-tags --separate-output --clear --title "Selección de Respaldos" \
+            --checklist "Use Espacio para seleccionar los respaldos y pulse OK cuando haya terminado." 0 0 0
         # INICIO RESPALDOS
         # FIN RESPALDOS
-        2>&1 >/dev/tty)
+        2>&1 >/dev/tty
+    )
     echo $OPTIONS >/tmp/oneline.tmp
-    
+
     # /tmp/oneline.tmp contiene los indices de los respaldos a borrar separados por espacios
     # Se crea un archivo llamado /tmp/newline.tmp que contiene los indices de los respaldos a borrar separados por saltos de linea
     awk '{for (i=1; i<=NF; i++) print $i}' /tmp/oneline.tmp >/tmp/newline.tmp
@@ -31,7 +33,7 @@ while true; do
         rm -f /tmp/list.tmp
         exit 1
     fi
-    
+
     # Pregrunta si se quiere eliminar los respaldos seleccionados
     dialog --title "Confirmación" --yesno "¿Está seguro que desea eliminar $eliminaciones respaldos?" 0 0
     if [ $? -eq 1 ]; then
@@ -41,18 +43,24 @@ while true; do
         exit 1
     fi
 
-    for ((i=eliminaciones; i>=1; i--)); do
-        # Con sed se elimina la linea i de /tmp/list.tmp
-        sed -i "$(</tmp/newline.tmp sed -n ${i}p)d" /tmp/list.tmp
+    for ((i = eliminaciones; i >= 1; i--)); do
+        # Con sed se elimina la linea i de tmp/list.tmp
+        sed -i "$(sed </tmp/newline.tmp -n ${i}p)d" /tmp/list.tmp
     done
+    read -p "Presione enter para continuar"
     rm -f /tmp/newline.tmp
-    
+
     # re escribir crontab
-    numeroInicio=$(grep -n "# INICIO RESPALDOS" /etc/crontab | cut -d: -f1)
-    numeroFin=$(grep -n "# FIN RESPALDOS" /etc/crontab | cut -d: -f1)
     head -$((numeroInicio)) /etc/crontab >/tmp/crontab
+    read -p "Presione enter para continuar"
     cat /tmp/list.tmp >>/tmp/crontab
+    read -p "Presione enter para continuar"
     tail -n +$((numeroFin)) /etc/crontab >>/tmp/crontab
+    read -p "Presione enter para continuar"
+    read -p "Presione enter para continuar"
+    read -p "Presione enter para continuar"
+    read -p "Presione enter para continuar"
+    read -p "Presione enter para continuar"
     rm -f /tmp/list.tmp
     mv /tmp/crontab /etc/crontab
     break
