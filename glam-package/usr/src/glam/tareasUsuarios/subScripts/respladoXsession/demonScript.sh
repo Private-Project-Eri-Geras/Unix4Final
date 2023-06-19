d466d271be972b3f88f84e847b461539a634e139
mariana-avila-rivera <marianaavilarivera@gmail.com>
2023-06-19 14:33:02 -0600

    Script del demonio que se encuentra en ejecución

--- glam-package/usr/src/glam/tareasUsuarios/subScripts/respladoXsession/demonScript.sh
+++ glam-package/usr/src/glam/tareasUsuarios/subScripts/respladoXsession/demonScript.sh
@@ -1,44 +1,69 @@
 #!/bin/bash
+# Ruta del archivo que guarda los inicios y terminos de sesión
+new=/var/glam/tmp/newWho.txt
+old=/var/glam/tmp/oldWho.txt
+diferencia=/var/glam/tmp/diff.txt
+nameArch="usuarios$(date +'%d%m%y').txt"
+ruta="/var/glam/logs/usrsInOut" 
+rutaUsrs="$ruta/$nameArch"
 
-# Función para manejar la acción de inicio de sesión
-function handle_login {
-    local user=$1
-    #echo "El usuario $user ha iniciado sesión" | wall
-    #who|grep "$user" >> "/usr/src/glam/tareasUsuarios/archs/usersInOUt.txt"
-    ./usr/src/glam/tareasUsuarios/subScripts/accionUsrIn.sh $user
-}
+  mkdir -p $ruta #verifica si existe la ruta, si no existe la crea
+  touch $rutaUsrs
 
-# Función para manejar la acción de cierre de sesión
-function handle_logout {
-    local user=$1
-    #echo "El usuario $user ha cerrado sesión" | wall
-    #who|grep "$user" >> "/usr/src/glam/tareasUsuarios/archs/usersInOUt.txt"
-    ./usr/src/glam/tareasUsuarios/subScripts/accionUsrOut.sh $user
-}
+##### CÓDIGO #####
 
-# Función principal del demonio
-function run_daemon {
-    local previous_users=()
-    while true; do
-        current_users=($(who | awk '{print $1}'))
-        
-        # Verifica si algún usuario ha iniciado sesión
-        for user in "${current_users[@]}"; do
-            if ! [[ " ${previous_users[@]} " =~ " ${user} " ]]; then
-                handle_login "$user"
-            fi
-        done
-        
-        # Verifica si algún usuario ha cerrado sesión
-        for user in "${previous_users[@]}"; do
-            if ! [[ " ${current_users[@]} " =~ " ${user} " ]]; then
-                handle_logout "$user"
-            fi
-        done
-        
-        previous_users=("${current_users[@]}")
-        sleep 1  # Espera 1 segundo antes de verificar nuevamente
-    done
-}
-run_daemon
+# Guardando la lista de usuarios conectados
+who > "$old"
 
+# Creando el cuadro de diálogo
+touch "$rutaUsrs"
+
+# Detección de inicios y terminos de sesión
+while true
+do
+   #Verifica que la fecha no haya cambiado, si lo hace crea un nuevo archivo
+    if [ "$nameArch" != "usuarios$(date +'%d%m%y').txt" ]; then
+        nameArch="usuarios$(date +'%d%m%y').txt"
+        rutaUsrs="$ruta/$nameArch"
+        touch $rutaUsrs
+    fi
+    #Elimina los archivos en la ruta que tengan más de 7 días
+    find $ruta -type f -mtime +7 -exec rm {} \;
+
+  who > "$new"
+  diff "$old" "$new" > "$diferencia"
+
+  # Obtener los usuarios que entran y mostrarlos en el cuadro de diálogo
+  usuarios_entran=$(awk '/>/ { print "in:   " $0 ; }' "$diferencia")
+  if [ -n "$usuarios_entran" ]; then
+    echo "$usuarios_entran" >> $rutaUsrs
+     #Obten el usuario que entró y ejecuta el script para revisar conf. de usuario
+    user=$(echo "$usuarios_entran" | awk '{print $3}')
+
+    #Verificar que el archivo exista
+    if [ -f "/usr/src/glam/tareasUsuarios/subScripts/accionUsrIn.sh" ]; then
+      ./usr/src/glam/tareasUsuarios/subScripts/accionUsrIn.sh $user
+    fi
+  fi
+
+  # Obtener los usuarios que salen y mostrarlos en el cuadro de diálogo
+  usuarios_salen=$(awk '/</ { print "out:  " $0 ; }' "$diferencia")
+  if [ -n "$usuarios_salen" ]; then
+      echo "$usuarios_salen" >> $rutaUsrs
+       #Obten el usuario que entró y ejecuta el script para revisar conf. de usuario
+    user=$(echo "$usuarios_salen" | awk '{print $3}')
+
+    #Verificar que el archivo exista
+    if [ -f "/usr/src/glam/tareasUsuarios/subScripts/accionUsrIn.sh" ]; then
+      ./usr/src/glam/tareasUsuarios/subScripts/accionUsrOut.sh $user
+    fi
+
+  fi
+
+  mv "$new" "$old"
+  sleep 1
+
+done
+rm $new
+rm $old
+rm $diferencia
