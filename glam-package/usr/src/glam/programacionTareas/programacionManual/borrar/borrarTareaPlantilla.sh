c4bdb30f0c00c9283ae676394db0ff42b4912f86
diosescalera <al217108@edu.uaa.mx>
2023-06-19 18:59:47 -0600

    huh

--- glam-package/usr/src/glam/programacionTareas/programacionManual/borrar/borrarTareaPlantilla.sh
+++ glam-package/usr/src/glam/programacionTareas/programacionManual/borrar/borrarTareaPlantilla.sh
@@ -4,7 +4,7 @@
 mostrar_ayuda() {
     dialog --title "Help" --msgbox \
         "\n\
-    Use Espacio para seleccionar los respaldos y pulse OK cuando haya terminado" 0 0
+    Use Espacio para seleccionar las tareas y pulse OK cuando haya terminado" 0 0
 }
 
 # Limpia la pantalla
@@ -13,14 +13,15 @@ clear
 # Imprime el menú usando dialog
 while true; do
     OPTIONS=$(dialog --no-tags --separate-output --clear --title "Selección de Respaldos" \
-            --checklist "Use Espacio para seleccionar los respaldos y pulse OK cuando haya terminado." 0 0 0
-        # INICIO RESPALDOS
-        # FIN RESPALDOS
+            --checklist "Use Espacio para seleccionar las tareas y pulse OK cuando haya terminado." 0 0 0
+        # INICIO TAREAS
+        # FIN TAREAS
         2>&1 >/dev/tty)
     echo $OPTIONS >/tmp/oneline.tmp
+    read -p "Presione enter para continuar"
 
-    # /tmp/oneline.tmp contiene los indices de los respaldos a borrar separados por espacios
-    # Se crea un archivo llamado /tmp/newline.tmp que contiene los indices de los respaldos a borrar separados por saltos de linea
+    # /tmp/oneline.tmp contiene los indices de las tareas a borrar separados por espacios
+    # Se crea un archivo llamado /tmp/newline.tmp que contiene los indices de las tareas a borrar separados por saltos de linea
     awk '{for (i=1; i<=NF; i++) print $i}' /tmp/oneline.tmp >/tmp/newline.tmp
     rm -f /tmp/oneline.tmp
     eliminaciones=$(cat /tmp/newline.tmp | wc -l)
@@ -33,7 +34,7 @@ while true; do
     fi
 
     # Pregrunta si se quiere eliminar los respaldos seleccionados
-    dialog --title "Confirmación" --yesno "¿Está seguro que desea eliminar $eliminaciones respaldos?" 0 0
+    dialog --title "Confirmación" --yesno "¿Está seguro que desea eliminar $eliminaciones tareas?" 0 0
     if [ $? -eq 1 ]; then
         clear
         rm -f /tmp/newline.tmp
@@ -41,7 +42,7 @@ while true; do
         exit 1
     fi
 
-    for ((i = eliminaciones; i >= 1; i--)); do
+    for ((i=eliminaciones; i>=1; i--)); do
         # Con sed se elimina la linea i de tmp/list.tmp
         sed -i "$(sed </tmp/newline.tmp -n ${i}p)d" /tmp/list.tmp
     done
