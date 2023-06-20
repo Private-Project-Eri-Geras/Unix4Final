e7065d1b536ec22114dc8454fa4768e907eb89a5
diosescalera <al217108@edu.uaa.mx>
2023-06-20 00:37:10 -0600

    todo en uno

---
+++ glam-package/usr/src/glam/programacionTareas/borradoTmpProgramado/selectivo/origen.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# Función para mostrar la ventana de ayuda
+mostrar_ayuda() {
+    dialog --title "Help" --msgbox \
+        "\n\
+    Para navegar se pude usar tab o las flechas.\n\n\
+    Es necesario usar la ruta absoluta del script.\n\n\
+    Al presionar espacio se autocopleta la ruta del archivo seleccionado." 0 0
+}
+
+while true; do
+    origen=$(dialog --title "Selecciona un temporal" \
+        --cancel-label "Cancelar" \
+        --help-button --help-label "Ayuda" \
+        --stdout --cursor-off-label --fselect /tmp/ 14 70)
+    opcion=$?
+
+    # Si el usuario presiona "Help" se sale del script
+    if [ $opcion -eq 2 ]; then
+        mostrar_ayuda
+
+    # Si el usuario presiona "Cancel" se muestra la ayuda
+    elif [ $opcion -eq 1 ]; then
+        break
+    # Si el usuario presiona "Slect"
+    elif [ $opcion -eq 0 ]; then
+        if [ -f "$origen" ]; then
+            dialog --title "CONFIRMAR" --yesno "¿Deseas usar el temporal $origen?" 0 0
+            opcion=$?
+            if [ $opcion -eq 0 ]; then
+                echo -n "rm -f $origen" >/tmp/origen.tmp
+                numeroFinSelectivo=$(grep -n "# FIN TMP SELECTIVO" /etc/crontab | cut -d ':' -f 1)
+                head -$((numeroFinSelectivo - 1)) /etc/crontab >/tmp/crontab
+                cat /tmp/cron.tmp /tmp/origen.tmp >>/tmp/crontab
+                echo "" >>/tmp/crontab
+                tail -n +${numeroFinSelectivo} /etc/crontab >>/tmp/crontab
+                rm /etc/crontab
+                mv /tmp/crontab /etc/crontab
+                dialog --title "" --msgbox "La tarea se ha creado exitosamente." 0 0
+                rm /tmp/cancelar.tmp
+                rm /tmp/cron.tmp
+                rm /tmp/dialogOutput.tmp
+                rm /tmp/Doutput.tmp
+                rm /tmp/output.tmp
+                rm /tmp/origen.tmp
+                clear
+                break
+            fi
+        elif [ -d "$origen" ]; then
+            dialog --title "CONFIRMAR" --yesno "¿Deseas usar el temporal $origen?" 0 0
+            opcion=$?
+            if [ $opcion -eq 0 ]; then
+                echo -n "rm -rf $origen" >/tmp/origen.tmp
+                numeroFinSelectivo=$(grep -n "# FIN TMP SELECTIVO" /etc/crontab | cut -d ':' -f 1)
+                head -$((numeroFinSelectivo - 1)) /etc/crontab >/tmp/crontab
+                cat /tmp/cron.tmp /tmp/origen.tmp >>/tmp/crontab
+                echo "" >>/tmp/crontab
+                tail -n +${numeroFinSelectivo} /etc/crontab >>/tmp/crontab
+                rm /etc/crontab
+                mv /tmp/crontab /etc/crontab
+                dialog --title "" --msgbox "La tarea se ha creado exitosamente." 0 0
+                rm /tmp/cancelar.tmp
+                rm /tmp/cron.tmp
+                rm /tmp/dialogOutput.tmp
+                rm /tmp/Doutput.tmp
+                rm /tmp/output.tmp
+                rm /tmp/origen.tmp
+                clear
+                break
+            fi
+        else
+            if [ -z "$origen" ]; then
+                dialog --title "ERROR" --msgbox "No se seleccionó nada a respaldar." 0 0
+            elif [ ! -f "$origen" ]; then
+                dialog --title "ERROR" --msgbox "El archivo seleccionado no existe." 0 0
+            fi
+        fi
+    fi
+
+done
